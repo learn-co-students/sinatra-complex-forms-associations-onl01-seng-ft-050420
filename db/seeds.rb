cbd0ea4e1970008522dc5f7f57726f43877b0450
Irene Scott <irenejinscott@gmail.com>
2020-06-24 13:49:34 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # Add seed data here. Seed your database with `rake db:seed`
+#seed file lets us write code that creates and saves instances of your models
 sophie = Owner.create(name: "Sophie")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
